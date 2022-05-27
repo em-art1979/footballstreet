40d4de5944c9c5acdfc42df10781adcea57d8a81
em-art1979 <106385285+em-art1979@users.noreply.github.com>
2022-05-27 19:57:31 +0200

    Add files via upload

---
+++ Balon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int fuerzaTiro;
+    public int fuerzaPase;
+    public bool IsControlling;
+    PlayerManager playerManager;
+    /*GameObject jugador2;*/
+
+    void Start()
+    {
+
+      IsControlling=false;
+
+      /*jugador2 = GameObject.Find("Player 2");*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(IsControlling==true)
+
+      {
+
+/*
+        playerManager=FindObjectOfType<PlayerManager>();
+        playerManager.IsPlayerSelected=true;
+*/
+
+        if(Input.GetKey(KeyCode.F))
+        {
+            GetComponent<Rigidbody>().AddForce(0,0,fuerzaTiro);
+        }
+        if(Input.GetKey(KeyCode.R))
+
+         {
+             GetComponent<Rigidbody>().velocity = transform.forward * 20;
+
+
+         }
+      }
+
+    }
+
+
+
+}
---
+++ Movimiento.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+
+    public float velocidad;
+    public float velocidadRotacion;
+
+    GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     ball = GameObject.Find("Ball");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Input.GetAxis("Horizontal") * velocidad, 0, Input.GetAxis("Vertical") * velocidad);
+        transform.LookAt(ball.transform);
+        /*transform.Rotate(0,Input.GetAxis("Horizontal") * velocidadRotacion, 0);*/
+
+
+    }
+
+}
