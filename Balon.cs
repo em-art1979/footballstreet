f9209b77b782b432d6c89dc2c28916bf8cda312f
em-art1979 <106385285+em-art1979@users.noreply.github.com>
2022-05-31 13:19:48 +0200

    Add files via upload

--- Balon.cs
+++ Balon.cs
@@ -7,44 +7,114 @@ public class Balon : MonoBehaviour
     // Start is called before the first frame update
     public int fuerzaTiro;
     public int fuerzaPase;
-    public bool IsControlling;
+    public bool IsControllingPlayer1;
+    public bool IsControllingPlayer2;
     PlayerManager playerManager;
-    /*GameObject jugador2;*/
+
+    GameObject jugador1;
+    GameObject jugador2;
 
     void Start()
     {
 
-      IsControlling=false;
-
-      /*jugador2 = GameObject.Find("Player 2");*/
+      IsControllingPlayer1=false;
+      IsControllingPlayer2=false;
+      jugador1 = GameObject.Find("Player 1");
+      jugador2 = GameObject.Find("Player 2");
     }
 
     // Update is called once per frame
     void Update()
     {
-      if(IsControlling==true)
+      if(IsControllingPlayer1==true)
 
       {
 
-/*
-        playerManager=FindObjectOfType<PlayerManager>();
-        playerManager.IsPlayerSelected=true;
-*/
+
+
 
         if(Input.GetKey(KeyCode.F))
         {
+
+
             GetComponent<Rigidbody>().AddForce(0,0,fuerzaTiro);
+
+
         }
+
+
+
+
         if(Input.GetKey(KeyCode.R))
 
-         {
-             GetComponent<Rigidbody>().velocity = transform.forward * 20;
+        {
 
 
-         }
+            GetComponent<Rigidbody>().velocity = transform.forward * 20;
+
+
+
+
+        }
+
+
+
+/*
+
+        else if(Input.GetKey(KeyCode.R))
+
+        {
+
+            transform.LookAt(jugador1.transform);
+
+
+        }
+
+*/
       }
 
+
+      if(IsControllingPlayer2==true)
+
+      {
+
+
+        if(Input.GetKey(KeyCode.F))
+        {
+
+
+            GetComponent<Rigidbody>().AddForce(0,0,fuerzaTiro);
+
+
+        }
+
+
+
+
+        if(Input.GetKey(KeyCode.R))
+
+        {
+
+            GetComponent<Rigidbody>().velocity = transform.forward * 20;
+
+
+        }
+/*
+        else if(Input.GetKey(KeyCode.R))
+
+        {
+
+          transform.LookAt(jugador2.transform);
+
+        }
+*/
+
+
+
     }
+
+
+  }
 
 
 
